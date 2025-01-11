bc9986a114a7ad9919932b4ebebbc95e6c7ad135
erelavni <2025arel.a@ebin.ort.org.il>
2025-01-12 00:16:10 +0200

    starting to get the hang of json files. fixed a few bugs in the testing folder. next commit should be the last of the testing and maybe even already include actual project progress.

--- test_json/test_util.py
+++ test_json/test_util.py
@@ -1,12 +1,7 @@
 import json
 import os
 
-def load_data_from_json(file_name : str, path = ""):
-    if path == "":
-        full_path = file_name
-    else:
-        full_path = os.path.join(file_name, path)
-
+def load_data_from_json(full_path: str):
     if not os.path.exists(full_path):
         print("The file does not exist. Returning empty dict.")
         return {}
@@ -18,18 +13,13 @@ def load_data_from_json(file_name : str, path = ""):
     return data
 
 
-def update_data_to_json(data: dict, file_name : str, path = ""):
-    if path == "":
-        full_path = file_name
-    else:
-        full_path = os.path.join(file_name, path)
-
+def update_data_to_json(data: dict, full_path: str):
     if not os.path.exists(full_path):
         print("The file does not exist. Not updating anything.")
         return
     
     data_key = data['id']
-    file_data = load_data_from_json(file_name, path)
+    file_data = load_data_from_json(full_path)
     file_data[data_key] = data
 
     with open(full_path, 'w') as file:
@@ -37,4 +27,25 @@ def update_data_to_json(data: dict, file_name : str, path = ""):
 
 
 if __name__ == "__main__":
-    print(load_data_from_json("test_chat_db.json"))
+    msg1 = {}
+    msg1['id'] = "1"
+    msg1['text'] = "this is the first message omg so exiting!!!"
+    msg1['sender'] = 1 #this is the sender's id
+
+    msg2 = {}
+    msg2['id'] = "2"
+    msg2['text'] = "this is the second message we're getting there"
+    msg2['sender'] = 1 #this is the sender's id
+
+    chat1 = {}
+    chat1['id'] = "1"
+    chat1['messages'] = ["1", "2"] #messages id
+    chat1['encryption_key'] = "the first key!"
+
+    user1 = {}
+    user1['id'] = "1"
+    user1['username'] = "kreker"
+    user1['chats'] = ["1"] #chats id
+
+    update_data_to_json(msg1, "C:\\Users\\משתמש\\Desktop\\TuneTogether\\TuneTogether\\test_json\\test_message_db.json")
+    
