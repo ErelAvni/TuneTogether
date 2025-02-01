2050b3957f08948af6207f7bdad68f9bea980946
erel avni <erelavni@outlook.com>
2025-02-01 20:48:26 +0200

    added TODO doc. Finished refactoring chat and group chat with from_dict and init changes. next up should be private chat then the rest of the classes

--- chat.py
+++ chat.py
@@ -5,16 +5,29 @@ import DButilites
 
 
 class Chat(ABC):
-    def __init__(self, chat_id: str, encryption_key: bytes = None):
+    def __init__(self, chat_id: str, encryption_key: bytes = None,
+                 messages: list = None):
+        """
+        Creates a new chat object.
+        Chiper is created with the encryption key, not a parameter.
+
+        :param chat_id: the unique chat id, provided by the subclasses
+        :param encryption_key: the encryption key for the chat, expected to be 32 bytes long. If not provided, a new key will be generated
+        :param messages: a list of the ids of the messages in the chat. If not provided, an empty list will be created
+        """
         self.__chat_id = chat_id
 
-        if encryption_key:# this is to handle logic of creaintg new chat with new encryption key (when the chat is created with insperation of a different chat)
+        if encryption_key:
             self.__encryption_key = encryption_key
         else:
             self.__encryption_key = Fernet.generate_key()
 
         self.__chiper = Fernet(self.__encryption_key)
-        self.__messages = []
+        
+        if messages:
+            self.__messages = messages
+        else:
+            self.__messages = []
     
     @property
     def chat_id(self):
@@ -34,19 +47,33 @@ class Chat(ABC):
     
 
     def add_message(self, msg: Message, path: str, data: dict):
+        """
+        Adds a message to the chat.
+        
+        :param msg: the message to be added
+        :param path: the path to the json file. Provided by the subclasses
+        """
         self.__messages.append(msg)
         DButilites.save_data_to_json(path, data)
     
 
     def decrypt_message(self, msg: Message) -> str:
+        """
+        returns the decrypted text of the message.
+        """
         decrypted_text_bytes = self.__chiper.decrypt(msg.text)
         return decrypted_text_bytes.decode()
     
     
     def to_dict(self):
+        """
+        Returns a dictionary representation of the chat.
+        When saving to a json file, this method should be used.
+        When saving a nested object, the id of said object is being saved.
+        Not saving the chiper object, as it is not serializable and there is no need to save it.
+        """
         return {
             'chat_id': self.chat_id,
-            'chiper': self.chiper,
             'encryption_key': self.encryption_key,
             'messages': [msg.message_id for msg in self.messages]
         }
--- groupChat.py
+++ groupChat.py
@@ -5,14 +5,35 @@ from message import Message
 
 
 class GroupChat(Chat):
-    def __init__(self, creator : User, name : str):
+    def __init__(self, name : str, creator : User, id : str = None,
+                  users : list = None, managers : dict = None,
+                    encryption_key : bytes = None, messages : list = None):
+        """
+        Creates a new group chat object. Params are in the following order:
+        :param name: the name of the chat
+        :param creator: the user that created the chat. Expected to be the user object, not the user id. Therefore, if using the from_dict method, the user object should be created first  
+        :param id: the unique chat id. If not provided, a new id will be generated. If provided it should be unique, in order to create an existing chat object
+        :param users: a list of the users (objects, not ids) in the chat. If not provided, an empty list will be created
+        :param managers: a dictionary with the user ids as keys and a boolean as value. If the value is True, the user is a manager. If not provided, the creator is the only manager
+        :param encryption_key: the encryption key for the chat, expected to be 32 bytes long. If not provided, a new key will be generated. This is a parameter of the parent class
+        :param messages: a list of the ids of the messages in the chat. If not provided, an empty list will be created. This is a parameter of the parent class
+        """
         last_id = DButilites.get_last_id(DButilites.GROUP_CHAT_DB_PATH)
         id = str(int(last_id) + 1)
-        super().__init__(id)
+        super().__init__(id, encryption_key, messages)
+
         self.__name = name
         self.__creator = creator
-        self.__users = [creator]
-        self.__managers = {creator.user_id: True} #this is in order to check if someone is manager or not in O(1) time complexity
+
+        if users:
+            self.__users = users
+        else:
+            self.__users = [creator]
+
+        if managers:
+            self.__managers = managers
+        else:
+            self.__managers = {creator.user_id: True} #this is in order to check if someone is manager or not in O(1) time complexity
 
 
     @property
@@ -36,18 +57,49 @@ class GroupChat(Chat):
 
 
     def add_message(self, msg: Message):
+        """
+        Adds a message to the chat. Gives the path for the group chat database and the data to be saved
+        """
         super().add_message(msg, DButilites.GROUP_CHAT_DB_PATH, self.to_dict())
 
 
     def add_user(self, user : User):
+        """
+        Adds a user to the chat. Saves the data to the group chat database
+        """
         self.__users.append(user)
         DButilites.save_data_to_json(DButilites.GROUP_CHAT_DB_PATH, self.to_dict())
 
 
     def to_dict(self):
+        """
+        Returns a dictionary with the chat data.
+        All nested obejcts are saved only by their ids, so if using this method to create an instance the nested objects should be created first.
+        """
         data_dict = super().to_dict()
+        data_dict['name'] = self.__name
         data_dict['creator'] = self.__creator.user_id
         data_dict['users'] = [self.__users.user_id for user in self.__users]
-        data_dict['users'] = self.__managers
+        data_dict['managers'] = self.__managers
         return data_dict
 
+
+    @classmethod
+    def from_dict(cls, data_dict : dict):
+        """
+        Creates a group chat object from a dictionary.
+        data_dict is expected to be the output of the to_dict method.
+        """
+        all_users = DButilites.load_data_from_json(DButilites.USER_DB_PATH)
+        creator = all_users[data_dict['creator']]
+        users = [all_users[user_id] for user_id in data_dict['users']]
+        managers = data_dict['managers']
+
+        name = data_dict['name']
+        chat_id = data_dict['chat_id']
+        encryption_key = data_dict['encryption_key']
+
+        all_messages = DButilites.load_data_from_json(DButilites.MESSAGE_DB_PATH)
+        messages = [all_messages[message_id] for message_id in data_dict['messages']]
+
+        return GroupChat(name, creator, chat_id, users, managers, encryption_key, messages)
